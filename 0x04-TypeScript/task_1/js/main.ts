114a5123f2d41457c3cbc7807f906adefcf36532
Authcornelius <corneliusedos@gmail.com>
2025-09-09 16:32:35 +0100

    yask 1 updated

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -38,7 +38,10 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
