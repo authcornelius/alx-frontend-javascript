d44894ef26cfb622c9394a5dd7bc7fdf88107127
Authcornelius <corneliusedos@gmail.com>
2025-09-11 11:44:26 +0100

    task 2 updated

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,10 +39,8 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  }
-  if (typeof salary === 'string' && Number(salary.replace(/[^0-9.-]+/g,"")) < 500) {
+  // @ts-ignore
+  if (salary < 500) {
     return new Teacher();
   }
   return new Director();
